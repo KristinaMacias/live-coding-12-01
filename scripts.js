96d7130c6f62115c322aa6f66111c285a64e67a0
KristinaMacias <maciaskristina@gmail.com>
2022-12-01 16:35:30 -0800

    pushing html and js files

---
+++ scripts.js
@@ -0,0 +1,24 @@
+console.log('Hello, Live-coding!');
+
+//var 
+//let
+//const
+
+let firstName = "Kristina";
+let lastName = "Macias";
+console.log(firstName + " " + lastName);
+
+lastName = "Ditmer";
+console.log(firstName + " " + lastName);
+
+const birthYear = 1990;
+console.log(birthYear);
+
+// birthYear = 1989;
+// console.log(birthYear);
+
+//show type of data
+console.log(typeof birthYear);
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
